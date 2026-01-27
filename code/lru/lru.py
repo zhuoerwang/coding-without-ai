4e102658a2ba75e22ff62b6fd37f9090fb7c8131
Joel Wang <joelwangusa@gmail.com>
2026-01-27 15:56:44 -0800

    fix pyright issues

--- code/lru/lru.py
+++ code/lru/lru.py
@@ -5,14 +5,16 @@ from collections.abc import Callable
 class Node():
     def __init__(self, key: int | None = None, val: int | None = None):
         self.key, self.val = key, val
-        self.prev, self.next = None, None
+        self.prev: Node | None = None
+        self.next: Node | None = None
 
 
 class LRUCache():
     def __init__(self, cap: int):
         self._cache = {}
         self._expire = {}
-        self._head, self._tail = Node(), Node()
+        self._head: Node = Node()
+        self._tail: Node = Node()
         self._head.next, self._tail.prev = self._tail, self._head
         self._cap = cap
         self._on_evict_cb = None
@@ -33,7 +35,7 @@ class LRUCache():
     def put(self, key: int, val: int, ttl: int | None = None) -> None:
         for k in list(self._expire):
             if time.time() > self._expire[k]:
-                self._evict_node(self._cache[k])        
+                self._evict_node(self._cache[k])
         
         if key in self._cache:
             self._remove(self._cache[key])
@@ -51,17 +53,19 @@ class LRUCache():
 
     def _move_to_end(self, node: Node):
         prev, nxt = self._tail.prev, self._tail
+        assert prev is not None
         node.prev, node.next = prev, nxt
         prev.next, nxt.prev = node, node
 
     def _remove(self, node: Node):
         prev, nxt = node.prev, node.next
+        assert prev is not None and nxt is not None
         nxt.prev, prev.next = prev, nxt
     
     def keys(self) -> list[int]:
         res = []
         node = self._tail.prev
-        while node != self._head:
+        while node is not None and node != self._head:
             if not (node.key in self._expire and time.time() > self._expire[node.key]):
                 res.append(node.key)
             node = node.prev
@@ -77,8 +81,11 @@ class LRUCache():
         
         return -1
 
-    def _evict_node(self, node: Node) -> None:
+    def _evict_node(self, node: Node | None) -> None:
+        if node is None: return
+
         if self._on_evict_cb:
+            assert node.key is not None and node.val is not None
             self._on_evict_cb(node.key, node.val)
         
         self._remove(node)
@@ -100,7 +107,7 @@ class LRUCache():
     def save(self, filepath: str) -> None:
         json_obj = {"cache": [], "cap": self._cap}
         node = self._head.next
-        while node != self._tail:
+        while node is not None and node != self._tail:
             json_obj["cache"].append((node.key, node.val))
             node = node.next
 
